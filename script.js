644245d525d11f13f75860b196ab98b05273964c
SRGuyYT <chapmlinks@gmail.com>
2025-08-26 18:21:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+// Dark mode toggle
+document.getElementById('toggle-dark').addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');
+});
+
+// Show modals
 function showLogin() {
   closeForms();
   document.getElementById('login-form').style.display = 'block';
+  document.getElementById('modal-bg').style.display = 'block';
 }
 function showSignup() {
   closeForms();
   document.getElementById('signup-form').style.display = 'block';
+  document.getElementById('modal-bg').style.display = 'block';
 }
 function showReset() {
   closeForms();
   document.getElementById('reset-form').style.display = 'block';
+  document.getElementById('modal-bg').style.display = 'block';
 }
 function closeForms() {
   document.getElementById('login-form').style.display = 'none';
   document.getElementById('signup-form').style.display = 'none';
   document.getElementById('reset-form').style.display = 'none';
+  document.getElementById('modal-bg').style.display = 'none';
 }
-
-document.getElementById('toggle-dark').addEventListener('click', () => {
-  document.body.classList.toggle('light-mode');
-});
